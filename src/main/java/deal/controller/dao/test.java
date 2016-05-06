4bb880324e5144962efc3769f8b06a9e946fed76
hhjhbhj <hhjhbhj@163.com>
2016-05-06 17:19:49 +0800

    test v1.0

---
+++ src/main/java/deal/controller/dao/test.java
@@ -0,0 +1,24 @@
+package deal.controller.dao;
+
+/**
+ * Created by lenovo on 2016/5/6.
+ */
+public class test {
+  private static final int t;
+
+  static {
+    try {
+      t = 1;
+    } catch (Exception e) {
+// TODO Auto-generated catch block
+      e.printStackTrace();
+      throw new Error();
+
+    }
+
+  }
+
+
+
+
+}
