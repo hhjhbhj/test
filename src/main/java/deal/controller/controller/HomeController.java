488df317930549bbf654243610aefea51f806546
hhjhbhj <hhjhbhj@163.com>
2016-06-02 15:01:22 +0800

    test v1.0  i18n  logs

--- src/main/java/deal/controller/controller/HomeController.java
+++ src/main/java/deal/controller/controller/HomeController.java
@@ -3,15 +3,21 @@ package deal.controller.controller;
 /**
  * Created by Administrator on 2016/1/4.
  */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
+  private static Logger logger = LoggerFactory.getLogger(HomeController.class);
   @RequestMapping("/home")
   public String home(Model model) {
+    logger.info ("home.jsp");
     model.addAttribute("message", "hello, world");
     //return "WEB-INF/views/home.jsp";
+
     return "home";
   }
 }
